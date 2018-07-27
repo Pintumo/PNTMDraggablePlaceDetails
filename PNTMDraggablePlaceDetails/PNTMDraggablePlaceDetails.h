b44757179ccaf6626b7ac1a6ae5a21f5695579ab
3vangelos <evangelos@posteo.eu>
2018-07-27 12:55:00 +0200

    Initial Commit

---
+++ PNTMDraggablePlaceDetails/PNTMDraggablePlaceDetails.h
@@ -0,0 +1,19 @@
+//
+//  PNTMDraggablePlaceDetails.h
+//  PNTMDraggablePlaceDetails
+//
+//  Created by Evangelos Sismanidis on 27.07.18.
+//  Copyright © 2018 Pinutmo UG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PNTMDraggablePlaceDetails.
+FOUNDATION_EXPORT double PNTMDraggablePlaceDetailsVersionNumber;
+
+//! Project version string for PNTMDraggablePlaceDetails.
+FOUNDATION_EXPORT const unsigned char PNTMDraggablePlaceDetailsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PNTMDraggablePlaceDetails/PublicHeader.h>
+
+
