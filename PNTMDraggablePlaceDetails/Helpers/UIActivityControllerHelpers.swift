ed34c0a927e8d0f866a53ad9070c1b2c3c501d97
3vangelos <evangelos@posteo.eu>
2018-08-23 12:21:01 +0200

    added some exclude Types to sharing

--- PNTMDraggablePlaceDetails/Helpers/UIActivityControllerHelpers.swift
+++ PNTMDraggablePlaceDetails/Helpers/UIActivityControllerHelpers.swift
@@ -4,8 +4,18 @@ extension UIActivityViewController {
     static func sharePlacesURL(_ url: URL, presenter: UIViewController) {
         let avc = UIActivityViewController(activityItems: [ url ], applicationActivities: nil)
         avc.popoverPresentationController?.sourceView = presenter.view
-        avc.excludedActivityTypes = [ .airDrop, .addToReadingList, .assignToContact, .markupAsPDF,
-                                      .openInIBooks, .postToVimeo, .saveToCameraRoll, .print ]
+        avc.excludedActivityTypes = [ .airDrop,
+                                      .addToReadingList,
+                                      .assignToContact,
+                                      .markupAsPDF,
+                                      .openInIBooks,
+                                      .postToVimeo,
+                                      .saveToCameraRoll,
+                                      .print,
+                                      .postToFlickr,
+                                      .postToVimeo,
+                                      UIActivity.ActivityType(rawValue: "com.apple.reminders.RemindersEditorExtension"),
+                                      UIActivity.ActivityType(rawValue: "com.apple.mobilenotes.SharingExtension")]
         presenter.present(avc, animated: true, completion: nil)
     }
 }
